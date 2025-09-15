9d5a5a4d7ef07c7b383fae3ca2f5220feca92613
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2025-09-15 17:16:41 +0200

    feat(advertisement): Add new advertisement creation and editing functionality

--- src/Controller/AdvertisementController.php
+++ src/Controller/AdvertisementController.php
@@ -7,6 +7,9 @@ use App\Repository\AdvertisementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\AdvertisementType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 final class AdvertisementController extends AbstractController
 {
@@ -20,10 +23,49 @@ final class AdvertisementController extends AbstractController
         ]);
     }
 
-    #[Route('/advertisement/{id}', name: 'advertisement_show', requirements: ['id' => '\d+'])]
+    #[Route('/advertisement/{id}', name: 'app_advertisement_show', requirements: ['id' => '\d+'])]
     public function show(Advertisement $advertisement): Response
     {
         return $this->render('advertisement/show.html.twig', [
+            'advertisement' => $advertisement,
+        ]);
+    }
+
+    #[Route('/advertisement/new', name: 'app_advertisement_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $advertisement = new Advertisement();
+        $form = $this->createForm(AdvertisementType::class, $advertisement);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($advertisement);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_advertisement');
+        }
+
+        return $this->render('advertisement/_form.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/advertisement/{id}/edit', name: 'app_advertisement_edit', requirements: ['id' => '\d+'])]
+    public function edit(Request $request, Advertisement $advertisement, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AdvertisementType::class, $advertisement);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_advertisement_show', ['id' => $advertisement->getId()]);
+        }
+
+        return $this->render('advertisement/_form.html.twig', [
+            'form' => $form,
             'advertisement' => $advertisement,
         ]);
     }
