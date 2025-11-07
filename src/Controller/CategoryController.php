296c4aba97d2796b6657e0d78848e099b912cf0e
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2025-11-07 10:38:54 +0100

    feat(CategoryController): Implement category listing and detail view

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,27 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAllAlphabetically();
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{id}', name: 'app_category_show')]
+    public function show(int $id, CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'advertisements' => $category->getAdvertisements(),
         ]);
     }
 }
